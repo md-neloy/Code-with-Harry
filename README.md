5472862aa22ac31666673d74934fccc5f9560ade
mh neloy <mhneloy708@gmail.com>
2023-08-09 12:40:24 +0600

    here i am work with making a text converting app & also work in enable dark mode button

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## this project is just nothing but it's updated day by day for knowladge purpose. knowladge about work with react. And here i'm working with some basic code and day by day i'm going it's advance knowladge
---
+++ src/components/From.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+export default function From(props) {
+  function capitalize(text) {
+    const word = text.split(" ");
+    if (word.length > 1) {
+      const capitalizeword = word.map(word=>
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      );
+      return capitalizeword.join(" ");
+    } else {
+      return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    }
+  }
+  const [text, setText] = useState("");
+  const handleupclick = () => {
+    console.log(text);
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  };
+  const handleLowclick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+  const handleCapiclick = () => {
+    console.log(text);
+    let uptext = capitalize(text);
+    setText(uptext);
+  };
+  
+  const onchange = (event) => {
+    console.log("upper click button");
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              <h2>{props.heading}</h2>
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              value={text}
+              onChange={onchange}
+              placeholder="write your text here"
+              rows="8"
+            ></textarea>
+          </div>
+          <div className="button" style={{ display: "flex", gap: "20px" }}>
+            <button className="btn btn-primary" onClick={handleupclick}>
+              Uppercase button
+            </button>
+            <button className="btn btn-primary" onClick={handleLowclick}>
+              Lowercase button
+            </button>
+            <button className="btn btn-primary" onClick={handleCapiclick}>
+              Capitalize button
+            </button>
+          </div>
+          <h3 className="my-2">your text summary</h3>
+          <p>{text.split(" ").length} word {text.length} letter</p>
+          <h3>Total time to read this text </h3>
+          <p>{ 0.008 * text.split(" ").length} minutes</p>
+        </div>
+      </section>
+    </>
+  );
+}
